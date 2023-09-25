e3a20c19702ea404d5fd85f86bd9003924f55e05
Soobum Kim <skim743@gatech.edu>
2023-09-25 11:48:12 -0400

    Updated docker_run.sh for mobiltarium demo

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -5,5 +5,5 @@ sudo docker run -d \
         	-e DISPLAY=$DISPLAY \
         	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
         	--net=host \
-        	--device /dev/video0:/dev/video0 \
+        	--device /dev/video4:/dev/video0 \
         	robotarium:python_tag_tracker
