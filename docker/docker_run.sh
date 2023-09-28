c539c786c298846fe30b4948327029558e390dcd
Soobum Kim <skim743@gatech.edu>
2023-09-27 20:27:15 -0400

    Added --rm option in docker_run.sh

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo docker run -d \
-	    --name robotarium_tag_tracker \
+			--rm \
+	    	--name robotarium_tag_tracker \
         	-e DISPLAY=$DISPLAY \
         	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
         	--net=host \
