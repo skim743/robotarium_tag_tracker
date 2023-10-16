35cf47060217a53a32f7cdf051ac3189b584f1b7
Soobum Kim <skim743@gatech.edu>
2023-10-16 17:25:51 -0400

    Testing local configs

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -7,4 +7,11 @@ sudo docker run -d \
         	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
         	--net=host \
         	--device /dev/video4:/dev/video0 \
-        	robotarium:python_tag_tracker
+        	robotarium:python_tag_tracker \
+			python3 robotarium_tag_tracker/tag_tracker/tracker.py \
+			--host '192.168.1.8' \
+			--port '1884' \
+			--calib robotarium_tag_tracker/config/camera_calib.yml \
+			--params robotarium_tag_tracker/config/detector_params.yml \
+			robotarium_tag_tracker/config/node_desc_tracker.json \
+			robotarium_tag_tracker/config/reference_markers_setup.yml
