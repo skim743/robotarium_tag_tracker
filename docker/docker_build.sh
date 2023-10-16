ab04d6167e3aab178cc23df7754809acc5d076a3
Soobum Kim <skim743@gatech.edu>
2023-10-16 18:02:10 -0400

    Finished testing local config files

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# if [ "$1" == "" ]; then
-# 	echo "First argument should be MQTT host IP (e.g., 192.168.1.8)"
-# 	exit
-# fi
-
-# if [ "$2" == "" ]; then
-# 	echo "Second argument should be MQTT host port (e.g., 1884)"
-# 	exit
-# fi
-
-
 (cd ../; sudo docker build --no-cache \
 		  --tag robotarium:python_tag_tracker \
 		  -f docker/Dockerfile .)
